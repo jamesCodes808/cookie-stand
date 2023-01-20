7d27a925a08f15c42ab414d941606a656a986687
jamesCodes808 <jamesian.r.solima@gmail.com>
2023-01-20 11:26:48 -0800

    class10-added new hours funcitonality ft.Reedooo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 'use strict'
 
-let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']
+// let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+let defaultHours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 
 // global variables 
 let storeLocationsList = [];
@@ -10,28 +12,66 @@ const salesTableFooterEl = document.getElementById('sales-table-footer');
 const salesDataFormEl = document.getElementById('sales-data-form')
 
 // constructor for salmon cookie shops 
-function Store(cityName, minCustomers, maxCustomers, avgCookiesSold) {
+function Store(cityName, minCustomers, maxCustomers, avgCookiesSold, openHours, closedHours) {
     this.cityName = cityName;
     this.minCustomers = minCustomers;
     this.maxCustomers = maxCustomers;
     this.avgCookiesSold = avgCookiesSold;
     this.listOfCookieSalesPerHour = [];
     this.dailySalesTotal = 0;
+    this.openHours = openHours;
+    this.closedHours = closedHours;
+    this.hours = [];
     storeLocationsList.push(this);
 }
 
 // sales data generator
 Store.prototype.getSalesData = function () {
-    for (let i = 0; i < hours.length; i++) {
+    for (let i = 0; i < defaultHours.length; i++) {
 
         let customersPerHour = Math.round((Math.random() * (this.maxCustomers - this.minCustomers) + this.minCustomers));
 
-        let cookieSales = Math.round(customersPerHour * this.avgCookiesSold);
+        let cookieSales;
 
+        console.log(`in the getsales data function: ${this.hours}`)
+
+        if (this.hours[i] == defaultHours[i]) {
+            cookieSales = Math.round(customersPerHour * this.avgCookiesSold);
+        } else {
+            cookieSales = 0;
+        }
         this.listOfCookieSalesPerHour.push(cookieSales)
+
         this.dailySalesTotal += cookieSales;
     }
 };
+
+Store.prototype.refactorHours = function () {
+    for (let i = this.openHours; i < this.closedHours; i++) {
+        if (i < 12) {
+            this.hours.push(i + ':00am')
+        }
+        else if (i == 12) {
+            this.hours.push(i + `:00pm`)
+        }
+        else if (i > 12) {
+            this.hours.push((i - 12) + ':00pm')
+        }
+    }
+    console.log(`this.hours In the refactor hours function${this.hours}`)
+    return this.hours;
+}
+
+Store.prototype.calculateCookiesForOpenHours = function () {
+    this.refactorHours();
+
+    for (var i = this.openHour; i < this.closeHour; i++) {
+        var cookiesEachHour = Math.round(this.avgCookiesPerCustomer * this.generateCustomersPerHour());
+        this.listOfCookieSalesPerHour.push(cookiesEachHour);
+    };
+    return this.cookiesPerHourArray;
+};
+
 
 Store.prototype.render = function () {
 
@@ -54,13 +94,15 @@ Store.prototype.render = function () {
 
 };
 
-new Store('seattle', 23, 65, 6.3);
-new Store('tokyo', 3, 24, 1.2);
-new Store('dubai', 11, 38, 3.7);
-new Store('paris', 20, 38, 2.3);
-new Store('lima', 2, 16, 4.6);
+new Store('seattle', 23, 65, 6.3, 6, 20);
+new Store('tokyo', 3, 24, 1.2, 6, 17);
+new Store('dubai', 11, 38, 3.7, 6, 15);
+new Store('paris', 20, 38, 2.3, 6, 14);
+new Store('lima', 2, 16, 4.6, 6, 19);
+
 
 for (let store of storeLocationsList) {
+    store.calculateCookiesForOpenHours();
     store.getSalesData();
     store.render();
 }
@@ -77,7 +119,7 @@ function renderTableFooter(storeLocationsArray) {
     totalsLabelEl.innerHTML = 'TOTALS'
 
     let dailyTotal = 0;
-    for (let i = 0; i < hours.length; i++) {
+    for (let i = 0; i < defaultHours.length; i++) {
         let hourlyTotal = 0;
         let cell = document.createElement('th');
         row.appendChild(cell);
@@ -97,14 +139,14 @@ function renderTableFooter(storeLocationsArray) {
 
 function renderHours() {
     // insert hour array
-    for (let i = 0; i < hours.length; i++) {
+    for (let i = 0; i < defaultHours.length; i++) {
         salesTableHeaderEl.append(document.createElement('th'));
     };
 
     let hourData = salesTableHeaderEl.querySelectorAll('th');
 
-    for (let i = 0; i < hours.length; i++) {
-        hourData[i].innerText = hours[i];
+    for (let i = 0; i < defaultHours.length; i++) {
+        hourData[i].innerText = defaultHours[i];
     };
 
     salesTableHeaderEl.prepend(document.createElement('th'));
@@ -122,11 +164,14 @@ salesDataFormEl.addEventListener('submit', (e) => {
     let minCust = e.target.minCust.value;
     let maxCust = e.target.maxCust.value;
     let avgCookies = e.target.avgCookies.value;
-    new Store(cityName, minCust, maxCust, avgCookies);
+    let openHours = e.target.openHours.value;
+    let closedHours = e.target.closedHours.value;
+    new Store(cityName, minCust, maxCust, avgCookies, openHours, closedHours);
     salesDataFormEl.reset();
 
     salesTableFooterEl.innerHTML = '';
 
+    storeLocationsList[storeLocationsList.length - 1].calculateCookiesForOpenHours();
     storeLocationsList[storeLocationsList.length - 1].getSalesData();
     storeLocationsList[storeLocationsList.length - 1].render();
 
